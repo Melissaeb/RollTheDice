d9dd0bc2e1fdf043032ede38a64664c816532310
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2025-07-09 16:55:34 +1200

    Rearrange layout

--- RollTheDice/ContentView.swift
+++ RollTheDice/ContentView.swift
@@ -18,28 +18,20 @@ struct ContentView: View {
     private let range: ClosedRange<Int> = 0...5
     
     var body: some View {
-        VStack {
-            VStack(spacing: 16) {
-                HStack {
-                    DiceView(number: 4, value: $totalD4s, range: range)
-                    ForEach(range.lowerBound..<totalD4s + 1, id: \.self) { total in
-                        if total > 0 {
-                            Text("\(total)")
-                        }
-                    }
-                }
+        NavigationStack {
+            VStack(spacing: 24) {
+                DiceView(number: 4, value: $totalD4s, range: range)
                 DiceView(number: 6, value: $totalD6s, range: range)
                 DiceView(number: 8, value: $totalD8s, range: range)
                 DiceView(number: 10, value: $totalD10s, range: range)
                 DiceView(number: 12, value: $totalD12s, range: range)
                 DiceView(number: 20, value: $totalD20s, range: range)
                 DiceView(number: 100, value: $totalD100s, range: range)
-            }
-            HStack {
                 
+                Text("Total: \(totalD4s + totalD6s + totalD8s + totalD10s + totalD12s + totalD20s + totalD100s)")
             }
+            .navigationTitle("Roll the Dice")
         }
-        .padding()
     }
 }
 
@@ -54,47 +46,61 @@ struct DiceView: View {
     let range: ClosedRange<Int>
     
     var body: some View {
-        VStack {
-            Text("D\(number)")
-                .fontWeight(.heavy)
-                .font(.headline)
-                .frame(width: 80, height: 40)
-                .foregroundStyle(.white)
-                .background(RoundedRectangle(cornerRadius: 4)
-                    .fill(Color.red))
-            HStack {
-                Button {
-                    if value > range.lowerBound {
-                        value -= 1
+        HStack {
+            HStack(spacing: 8) {
+                Text("D\(number)")
+                    .fontWeight(.heavy)
+                    .frame(width: 60, height: 40)
+                    .foregroundStyle(.white)
+                    .background(RoundedRectangle(cornerRadius: 4)
+                        .fill(Color.red))
+                    .padding(0)
+                VStack {
+                    Button {
+                        if value < 5 {
+                            value += 1
+                        }
+                        print(value)
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 12, height: 12)
+                            .foregroundColor(value < range.upperBound ? .primary.opacity(0.9) : .gray)
+                            .frame(width: 20, height: 20)
+                            .background(Color.secondary.opacity(0.2))
+                            .clipShape(RoundedRectangle(cornerRadius: 4))
                     }
-                } label: {
-                    Image(systemName: "minus")
-                        .resizable()
-                        .frame(width: 12, height: 1.5)
-                        .foregroundColor(value > range.lowerBound ? .primary.opacity(0.9) : .gray)
-                        .frame(width: 20, height: 20)
-                        .background(Color.secondary.opacity(0.2))
-                        .clipShape(RoundedRectangle(cornerRadius: 4))
-                }
-                
-                Text("\(value)")
-                    .font(.caption)
-                
-                Button {
-                    if value < 5 {
-                        value += 1
+                    Button {
+                        if value > range.lowerBound {
+                            value -= 1
+                        }
+                    } label: {
+                        Image(systemName: "minus")
+                            .resizable()
+                            .frame(width: 12, height: 1.5)
+                            .foregroundColor(value > range.lowerBound ? .primary.opacity(0.9) : .gray)
+                            .frame(width: 20, height: 20)
+                            .background(Color.secondary.opacity(0.2))
+                            .clipShape(RoundedRectangle(cornerRadius: 4))
                     }
-                    print(value)
-                } label: {
-                    Image(systemName: "plus")
-                        .resizable()
-                        .frame(width: 12, height: 12)
-                        .foregroundColor(value < range.upperBound ? .primary.opacity(0.9) : .gray)
-                        .frame(width: 20, height: 20)
-                        .background(Color.secondary.opacity(0.2))
-                        .clipShape(RoundedRectangle(cornerRadius: 4))
                 }
             }
+            .padding(.horizontal)
+            
+            HStack {
+                ForEach(range.lowerBound...value, id: \.self) { value in
+                    if value > 0 {
+                        Text("\(number)")
+                            .fontWeight(.heavy)
+                            .frame(width: 32, height: 32)
+                            .background(Color.purple)
+                            .clipShape(Circle())
+                            .foregroundStyle(.white)
+                    }
+                }
+                
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
