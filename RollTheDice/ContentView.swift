3de75ba954eac53027f1cd03146dcb03b50a1c37
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2025-07-10 18:23:02 +1200

    Refactor

--- RollTheDice/ContentView.swift
+++ RollTheDice/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diceRolled: Bool = false
     @State private var totalD4s: Int = 0
     @State private var totalD6s: Int = 0
     @State private var totalD8s: Int = 0
@@ -17,21 +18,86 @@ struct ContentView: View {
     @State private var totalD100s: Int = 0
     private let range: ClosedRange<Int> = 0...5
     
+    @State private var d4Rolls: [Int] = []
+    @State private var d6Rolls: [Int] = []
+    @State private var d8Rolls: [Int] = []
+    @State private var d10Rolls: [Int] = []
+    @State private var d12Rolls: [Int] = []
+    @State private var d20Rolls: [Int] = []
+    @State private var d100Rolls: [Int] = []
+    
     var body: some View {
         NavigationStack {
             VStack(spacing: 24) {
-                DiceView(number: 4, value: $totalD4s, range: range)
-                DiceView(number: 6, value: $totalD6s, range: range)
-                DiceView(number: 8, value: $totalD8s, range: range)
-                DiceView(number: 10, value: $totalD10s, range: range)
-                DiceView(number: 12, value: $totalD12s, range: range)
-                DiceView(number: 20, value: $totalD20s, range: range)
-                DiceView(number: 100, value: $totalD100s, range: range)
+                DiceView(number: 4, totalDice: $totalD4s, range: range, diceRolls: $d4Rolls, diceRolled: $diceRolled)
+                DiceView(number: 6, totalDice: $totalD6s, range: range, diceRolls: $d6Rolls, diceRolled: $diceRolled)
+                DiceView(number: 8, totalDice: $totalD8s, range: range, diceRolls: $d8Rolls, diceRolled: $diceRolled)
+                DiceView(number: 10, totalDice: $totalD10s, range: range, diceRolls: $d10Rolls, diceRolled: $diceRolled)
+                DiceView(number: 12, totalDice: $totalD12s, range: range, diceRolls: $d12Rolls, diceRolled: $diceRolled)
+                DiceView(number: 20, totalDice: $totalD20s, range: range, diceRolls: $d20Rolls, diceRolled: $diceRolled)
+                DiceView(number: 100, totalDice: $totalD100s, range: range, diceRolls: $d100Rolls, diceRolled: $diceRolled)
                 
-                Text("Total: \(totalD4s + totalD6s + totalD8s + totalD10s + totalD12s + totalD20s + totalD100s)")
+                HStack(spacing: 40) {
+                    Button("ROLL", action: rollDice)
+                        .frame(width: 120, height: 40)
+                        .background(Color.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                    Text("Total: \(d4Rolls.reduce(0, +) + d6Rolls.reduce(0, +) + d8Rolls.reduce(0, +) + d10Rolls.reduce(0, +) + d12Rolls.reduce(0, +) + d20Rolls.reduce(0, +) + d100Rolls.reduce(0, +))")
+                }
+                .padding(.bottom, 24)
             }
+            .padding(.top, 16)
             .navigationTitle("Roll the Dice")
         }
+    }
+    
+    func rollDice() {
+        d4Rolls.removeAll()
+        d6Rolls.removeAll()
+        d8Rolls.removeAll()
+        d10Rolls.removeAll()
+        d12Rolls.removeAll()
+        d20Rolls.removeAll()
+        d100Rolls.removeAll()
+        
+        if self.totalD4s > 0 {
+            for _ in 1...self.totalD4s {
+                d4Rolls.append(Int.random(in: 1...4))
+            }
+        }
+        if self.totalD6s > 0 {
+            for _ in 1...self.totalD6s {
+                d6Rolls.append(Int.random(in: 1...6))
+            }
+        }
+        if self.totalD8s > 0 {
+            for _ in 1...self.totalD8s {
+                d8Rolls.append(Int.random(in: 1...8))
+            }
+        }
+        if self.totalD10s > 0 {
+            for _ in 1...self.totalD10s {
+                d10Rolls.append(Int.random(in: 1...10))
+            }
+        }
+        if self.totalD12s > 0 {
+            for _ in 1...self.totalD12s {
+                d12Rolls.append(Int.random(in: 1...12))
+            }
+        }
+        if self.totalD20s > 0 {
+            for _ in 1...self.totalD20s {
+                d20Rolls.append(Int.random(in: 1...20))
+            }
+        }
+        if self.totalD100s > 0 {
+            for _ in 1...self.totalD100s {
+                d100Rolls.append(Int.random(in: 1...100))
+            }
+        }
+        diceRolled = true
     }
 }
 
@@ -42,8 +108,10 @@ struct ContentView: View {
 
 struct DiceView: View {
     let number: Int
-    @Binding var value: Int
+    @Binding var totalDice: Int
     let range: ClosedRange<Int>
+    @Binding var diceRolls: [Int]
+    @Binding var diceRolled: Bool
     
     var body: some View {
         HStack {
@@ -57,50 +125,49 @@ struct DiceView: View {
                     .padding(0)
                 VStack {
                     Button {
-                        if value < 5 {
-                            value += 1
+                        if totalDice < 5 {
+                            totalDice += 1
+                            diceRolled = false
                         }
-                        print(value)
                     } label: {
                         Image(systemName: "plus")
                             .resizable()
                             .frame(width: 12, height: 12)
-                            .foregroundColor(value < range.upperBound ? .primary.opacity(0.9) : .gray)
+                            .foregroundColor(totalDice < range.upperBound ? .primary.opacity(0.9) : .gray)
                             .frame(width: 20, height: 20)
                             .background(Color.secondary.opacity(0.2))
                             .clipShape(RoundedRectangle(cornerRadius: 4))
                     }
                     Button {
-                        if value > range.lowerBound {
-                            value -= 1
+                        if totalDice > range.lowerBound {
+                            totalDice -= 1
+                            diceRolled = false
                         }
                     } label: {
                         Image(systemName: "minus")
                             .resizable()
                             .frame(width: 12, height: 1.5)
-                            .foregroundColor(value > range.lowerBound ? .primary.opacity(0.9) : .gray)
+                            .foregroundColor(totalDice > range.lowerBound ? .primary.opacity(0.9) : .gray)
                             .frame(width: 20, height: 20)
                             .background(Color.secondary.opacity(0.2))
                             .clipShape(RoundedRectangle(cornerRadius: 4))
                     }
                 }
             }
-            .padding(.horizontal)
+            .padding(.leading)
             
             HStack {
-                ForEach(range.lowerBound...value, id: \.self) { value in
-                    if value > 0 {
-                        Text("\(number)")
-                            .fontWeight(.heavy)
-                            .frame(width: 40, height: 40)
-                            .background(Color.purple)
-                            .clipShape(Circle())
-                            .foregroundStyle(.white)
-                    }
+                ForEach(0..<totalDice, id: \.self) { index in
+                    Text(diceRolled && index < diceRolls.count ? "\(diceRolls[index])" : "\(number)")
+                        .fontWeight(.heavy)
+                        .frame(width: 40, height: 40)
+                        .background(Color.purple)
+                        .clipShape(Circle())
+                        .foregroundStyle(.white)
                 }
-                
             }
             .frame(maxWidth: .infinity)
+            .padding(.trailing)
         }
     }
 }
